31847d22998a0bac3fd7a9bbf9ae5e47f7cfaf27
Masataka Takaki <oorgns1uw5sk3ae@gmail.com>
2024-01-08 20:25:37 +0900

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,27 @@
+<script>
+  const regionNamesInJapan = new Intl.DisplayNames(['ja'], { type: 'region' });
+  const regionNamesInTraditionalChinese = new Intl.DisplayNames(['zh-Hant'], { type: 'region' });
+  console.log(regionNamesInTraditionalChinese.of('ES'));
+  console.log(regionNamesInJapan.of('ES'));
+  // console.log(regionNamesInTraditionalChinese.of('UK'));
+  // console.log(regionNamesInJapan.of('UK'));
+  // console.log(regionNamesInTraditionalChinese.of('DE'));
+  // console.log(regionNamesInJapan.of('DE'));
+  // console.log(regionNamesInTraditionalChinese.of('JP'));
+  // console.log(regionNamesInTraditionalChinese.of('KR'));
+  // console.log(regionNamesInJapan.of('KR'));
+  // console.log(regionNamesInTraditionalChinese.of('RU'));
+  // console.log(regionNamesInJapan.of('RU'));
+  // console.log(regionNamesInTraditionalChinese.of('SD'));
+  // console.log(regionNamesInJapan.of('SD'));
+  // console.log(regionNamesInTraditionalChinese.of('SV'));
+  // console.log(regionNamesInJapan.of('SV'));
+</script>
+
+<main>
+  
+</main>
+
+<style>
+  
+</style>
