25b3f168db738297c69efbd8bf2a54255e8bb803
Masataka Takaki <oorgns1uw5sk3ae@gmail.com>
2024-01-12 00:03:27 +0900

    add anchor link

--- src/App.svelte
+++ src/App.svelte
@@ -1,34 +1,17 @@
 <script>
-  import Header from "./components/Header.svelte";
-  import CountryList from "./components/CountryList.svelte";
-  import Footer from "./components/Footer.svelte";
+  // @ts-ignore
+  import { Router, Route } from "svelte-routing";
 
-  const regionNamesInJapan = new Intl.DisplayNames(['ja'], { type: 'region' });
-  const regionNamesInTraditionalChinese = new Intl.DisplayNames(['zh-Hant'], { type: 'region' });
-  // console.log(regionNamesInTraditionalChinese.of('ES'));
-  // console.log(regionNamesInJapan.of('ES'));
-  // console.log(regionNamesInTraditionalChinese.of('UK'));
-  // console.log(regionNamesInJapan.of('UK'));
-  // console.log(regionNamesInTraditionalChinese.of('DE'));
-  // console.log(regionNamesInJapan.of('DE'));
-  // console.log(regionNamesInTraditionalChinese.of('JP'));
-  // console.log(regionNamesInTraditionalChinese.of('KR'));
-  // console.log(regionNamesInJapan.of('KR'));
-  // console.log(regionNamesInTraditionalChinese.of('RU'));
-  // console.log(regionNamesInJapan.of('RU'));
-  // console.log(regionNamesInTraditionalChinese.of('SD'));
-  // console.log(regionNamesInJapan.of('SD'));
-  // console.log(regionNamesInTraditionalChinese.of('SV'));
-  // console.log(regionNamesInJapan.of('SV'));
+  import Home from "./routes/Home.svelte";
+  import CountryList from "./routes/CountryList.svelte";
+  import Opinion from "./routes/Opinion.svelte";
+  export let url = "";
 </script>
+<Router {url}>
+  <div>
+    <Route path="/" component={Home}/>
+    <Route path="/countrylist" component={CountryList} />
+    <Route path="/opinion" component={Opinion} />
+  </div>
+</Router>
 
-<Header/>
-<main>
-  <h1>How do you say this country in china???</h1>
-  <CountryList/>
-</main>
-<Footer/>
-
-<style>
-  
-</style>
---
+++ src/routes/Opinion.svelte
@@ -0,0 +1,21 @@
+<!-- +++++ js +++++ -->
+<script>
+    import Footer from "../components/Footer.svelte";
+    import Header from "../components/Header.svelte";
+
+
+</script>
+<!-- +++++ end +++++ -->
+<!-- end -->
+<!-- +++++ html +++++ -->
+    <Header/>
+    <main>
+        <h1>Opinion</h1>
+    </main>
+    <Footer/>
+<!-- +++++ end +++++ -->
+<!-- +++++ css +++++ -->
+<style>
+    
+</style>
+<!-- +++++ end +++++ -->
