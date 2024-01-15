2fd57a95af5e16d76217d4f09ffa58579dde0819
Masataka Takaki <oorgns1uw5sk3ae@gmail.com>
2024-01-15 23:23:56 +0900

    add opinion page

--- src/routes/Opinion.svelte
+++ src/routes/Opinion.svelte
@@ -2,8 +2,6 @@
 <script>
     import Footer from "../components/Footer.svelte";
     import Header from "../components/Header.svelte";
-
-
 </script>
 <!-- +++++ end +++++ -->
 <!-- end -->
@@ -11,11 +9,19 @@
     <Header/>
     <main>
         <h1>Opinion</h1>
+        <p>ご意見・ご感想が御座いましたら下記からお願いします！</p>
+        <a href="https://docs.google.com/forms/d/e/1FAIpQLSdz6slEfMqmVm62dG3kdjelVHotNqlWa_S6wetiVK0O1aPICg/viewform?usp=sf_link">こちらから！</a>
     </main>
     <Footer/>
 <!-- +++++ end +++++ -->
 <!-- +++++ css +++++ -->
 <style>
-    
+    main {
+        text-align: center;
+    }
+
+    main * {
+        margin-bottom: 24px;
+    }
 </style>
 <!-- +++++ end +++++ -->
